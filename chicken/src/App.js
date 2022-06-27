61ea4002a465b9c694e6e60173874971f32a2059
Kalyani <kalyanisab9@gmail.com>
2022-06-27 17:27:22 -0600

    update  sign in and add to cart buttons

---
+++ chicken/src/App.js
@@ -0,0 +1,71 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <head>
+
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"></link>    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    </head>
+     <body>
+      
+       
+       
+          
+            <div class="col">
+              <div class = "nav">
+                <ul>
+                  <li><a href="home.html">HOME</a></li>
+                  <li><a href="menu.html">MENU</a>
+                     <ul>
+                      <li><a href="chicken.html">Chicken</a></li>
+                       <li><a href="shrimp.html">Shrimp</a></li>
+                       <li><a href="sides.html">Sides</a></li>
+                       <li><a href="drinks.html">Drinks</a></li>
+                     </ul>
+                  </li>
+                
+                
+                  <li><a href="calories.html">NUTRIENTS</a></li>
+                  <li><a href="contact.html">CONTACT US</a></li>
+                  <li><h1>Chic*Chicken</h1></li>
+                </ul>
+              </div>
+            
+            
+                 <div class="nav-right">
+                  <ul>
+                  <li><a href="signin.html"><button type="button" class="btn btn-warning">Sign In</button></a></li>
+                   <li><a href="cart.html"></a><button class="btn btn-warning" type="submit"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16">
+                   <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+                   </svg>
+                   </button>
+                 </li>
+               </ul>
+            
+              
+            </div>
+          </div>
+          
+            
+             
+                
+                 
+                  
+                  
+             
+           </body> 
+        
+        </header>
+    
+        
+        </div>
+    
+  );
+}
+
+export default App;
